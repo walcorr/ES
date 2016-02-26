57365a51c2d6c2ba35df7e2b0e486dedcaa6732b
walelite <walcorr@gmail.com>
2016-02-26 16:01:48 -0300

    Added files via upload

---
+++ MinMaxRecursivo.py
@@ -0,0 +1,19 @@
+def minmax(l):
+    if len(l)==0:
+        return None
+    if len(l)==1:
+        return l[0],l[0]
+    else:
+        return MaxMin(l,len(l),l[0],l[0])
+
+def MaxMin(l,t,mi,ma):
+    if mi<l[t-1]:
+        mi=l[t-1]
+    if ma>l[t-1]:
+        ma=l[t-1]
+    if t==1:
+        return mi,ma
+    else:
+        return MaxMin(l,t-1,mi,ma)
+
+#tempo O(n) pois vai percorrer a lista inteira. Memória O(n)
