d465c43453a6de6d41f14d3a2d69e52baec5abdb
walelite <walcorr@gmail.com>
2016-03-26 17:45:44 -0300

    Update Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,5 +1,5 @@
 import unittest
-def ordenar(seq):
+def bubble_sort(seq):
 
     '''
     para memória O(1) pois possuirá apenas a lista e uma variavel apenas
@@ -22,17 +22,17 @@ def ordenar(seq):
 
 class OrdenacaoTestes(unittest.TestCase):
     def teste_lista_vazia(self):
-        self.assertListEqual([], ordenar([]))
+        self.assertListEqual([], bubble_sort([]))
 
     def teste_lista_unitaria(self):
-        self.assertListEqual([1], ordenar([1]))
+        self.assertListEqual([1], bubble_sort([1]))
 
     def teste_lista_binaria(self):
-        self.assertListEqual([1, 2], ordenar([2, 1]))
+        self.assertListEqual([1, 2], bubble_sort([2, 1]))
 
 
     def teste_lista_binaria(self):
-        self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ordenar([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
+        self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], bubble_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
 
 
 if __name__ == '__main__':
